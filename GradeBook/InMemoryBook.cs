951fce77ab68783a070b0178fc058905456e2ba9
Venkata Sreyanth <bsreyanth@ariqt.com>
2024-04-14 10:21:06 +0530

    This is gradebook console

---
+++ GradeBook/InMemoryBook.cs
@@ -0,0 +1,165 @@
+﻿using GradeBook;
+
+namespace GradeBook
+{
+    public class NameObject
+    {
+        public NameObject(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+    public abstract class Book : NameObject, IBook
+    {
+        protected Book(string name) : base(name)
+        {
+        }
+        public abstract event GradeAddedDelegate GradeAdded;
+        public abstract void AddGrade(double grade);
+        public abstract Statistics GetStatistics();
+
+    }
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) : base(name) { }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        // This method is storing all the grades that have been entered to a text document.
+        public override void AddGrade(double grade)
+        {
+            using (var writer = File.AppendText($"{Name}.txt"))
+            {
+
+                writer.WriteLine(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs()); // Raising the event 
+                }
+            }
+
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            using (var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while (line != null && line != "")
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
+        }
+    }
+
+}
+public delegate void GradeAddedDelegate(object sender, EventArgs e);
+public class InMemoryBook : Book
+{
+    public override event GradeAddedDelegate GradeAdded;
+
+    private List<double> grades;
+
+    /*readonly int a = 1;*/ // while using readonly we can declare the variable in the sameline or in the constructor  
+    /* const string CATEGORY = "science"; */// the value of const can't be changed and the all the alphabets should be in capital letters.
+    public InMemoryBook(string name) : base(name)
+    {
+        /*a++;*/
+
+
+        grades = new List<double>();
+        Name = name;
+
+    }
+
+    // This method is taking values which are of type char.
+    public void AddGrade(char letter)
+    {
+        switch (letter)
+        {
+            case 'A':
+                AddGrade(90);
+                break;
+
+            case 'B':
+                AddGrade(80);
+                break;
+
+            case 'C':
+                AddGrade(70);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // This is a overridden method which is a good example of Runtime polymorphism
+    public override void AddGrade(double grade)
+    {
+        if (grade <= 100 && grade >= 0)
+        {
+            grades.Add(grade);
+
+            if (GradeAdded != null)
+            {
+                GradeAdded(this, new EventArgs());
+            }
+
+        }
+        else
+        {
+            throw new ArgumentException($"Invalid {nameof(grade)}");
+        }
+    }
+
+
+
+    public override Statistics GetStatistics()
+    {
+
+        Statistics s1 = new();
+
+
+        for (var index = 0; index < grades.Count; index++)
+        {
+            s1.Add(grades[index]);
+
+            /*if (grades[index] == 65.2)
+            {
+                break;
+            }*/
+
+            /*if (grades[index] == 65.2)
+            {
+                continue;
+            }*/
+
+            /* if (grades[index] == 65.2)
+             {
+                 goto over;
+             }*/
+
+
+        }
+        /* over:*/
+        return s1;
+
+    }
+}
+
---
+++ GradeBook/Program.cs
@@ -0,0 +1,72 @@
+﻿using GradeBook;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Creating an Object of Book */
+        IBook book = new DiskBook("Sreyanth Grade Book");
+
+        book.GradeAdded += OnGradeAdded;
+
+        GradeAddition(book);
+
+
+        /* Adding all the grades to list */
+        /*book.AddGrade(56.10);
+        book.AddGrade(89.1);
+        book.AddGrade(90.5);
+        book.AddGrade(77.5);*/
+
+        /* invoking ShowStatistics method */
+
+        var stats = book.GetStatistics();
+
+        Console.WriteLine(book.Name);
+        Console.WriteLine($"The average is {stats.Average:N2}");
+        Console.WriteLine($"The high score is {stats.High}");
+        Console.WriteLine($"The Low score is {stats.Low}");
+        Console.WriteLine($"The grade is {stats.Letter}");
+
+    }
+
+    private static void GradeAddition(IBook book)
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter a grade or q to quit!!");
+            var grade = Console.ReadLine();
+
+            if (grade == "q" || grade == "Q")
+            {
+                break;
+            }
+
+            try
+            {
+                book.AddGrade(double.Parse(grade));
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                /*Console.WriteLine("You are in Finally block");*/
+            }
+
+        }
+    }
+
+    static void OnGradeAdded(object sender, EventArgs e)
+    {
+
+        Console.WriteLine("A Grade was Added");
+    }
+
+}
